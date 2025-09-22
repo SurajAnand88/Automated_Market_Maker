fec37bef916f9234bcdd83b1f4c32c019e915ca6
SurajAnand88 <surajbloodstone@gmail.com>
2025-09-22 11:25:03 +0530

    added addLiquidity function

--- src/LiquidityPool.sol
+++ src/LiquidityPool.sol
@@ -2,10 +2,16 @@
 pragma solidity ^0.8.4;
 
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import {Math} from "../lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
 
 contract LiquidityPool {
+    using Math for uint112;
+
     address public immutable tokenA;
     address public immutable tokenB;
+
+    bool private firstTime;
+    uint256 private poolRatio;
 
     uint112 public reserveTokenA;
     uint112 public reserveTokenB;
@@ -14,12 +20,16 @@ contract LiquidityPool {
     string public constant SYMBOL = "LST";
     uint256 public constant DECIMAL = 18;
     uint256 public totalSupply;
+    uint112 private constant MINIMUM_LIQUIDITY = 10 ** 3;
 
     mapping(address owner => uint256 amount) balances;
     mapping(address owner => mapping(address spender => uint256 amount)) allowances;
 
+    // <------------------ERRORS------------------>
+    error LiquidityPool_DepositeRatioShouldBeSimilar();
+
     // <------------------EVENTS------------------>
-    event Mint(uint256 indexed provider, address tokenA, address tokenB, uint256 liquidity);
+    event Mint(address indexed provider, uint112 tokenA, uint112 tokenB, uint256 liquidity);
     event Burn(uint256 indexed provider, address tokenA, address tokenB, uint256 liquidity);
     event Swap(uint256 indexed swapper, uint256 amountIn, uint256 amountOut, address tokenIn, address tokenOut);
     event Transfer(address indexed from, address indexed to, uint256 indexed value);
@@ -81,7 +91,66 @@ contract LiquidityPool {
         return true;
     }
 
-    function addLiquidity() public {}
+    function addLiquidity(uint112 amountADesired, uint112 amountBDesired)
+        public
+        returns (uint112 amountA, uint112 amountB, uint256 liquidity)
+    {
+        //calculate the optional DesiredToken
+        if (reserveTokenA > 0 && reserveTokenB > 0) {
+            uint112 amountBoptimal = (amountADesired * reserveTokenB) / reserveTokenA;
+            if (amountBoptimal <= amountBDesired) {
+                amountA = amountADesired;
+                amountB = amountBoptimal;
+            } else {
+                uint112 amountAoptimal = (amountBDesired * reserveTokenA) / reserveTokenB;
+                if (amountAoptimal <= amountADesired) {
+                    amountB = amountBDesired;
+                    amountA = amountAoptimal;
+                }
+            }
+        } else {
+            amountA = amountADesired;
+            amountB = amountBDesired;
+        }
+
+        IERC20(tokenA).transferFrom(msg.sender, address(this), amountADesired);
+        IERC20(tokenB).transferFrom(msg.sender, address(this), amountBDesired);
+
+        if (liquidity == 0) {
+            liquidity = uint256(Math.sqrt(amountA * amountB) - MINIMUM_LIQUIDITY);
+            _mint(address(0), liquidity);
+        } else {
+            liquidity = Math.min((amountA * totalSupply) / reserveTokenA, (amountB * totalSupply) / reserveTokenB);
+        }
+        require(liquidity > 0, "Insufficient LP Tokens to mint");
+        _mint(msg.sender, liquidity);
+        _updateReserves(amountA, amountB);
+        emit Mint(msg.sender, amountA, amountB, liquidity);
+    }
+
+    function _calculateLpTokens(uint112 amountAToken, uint112 amountBToken) public pure returns (uint256) {}
+
+    function _calculateTokenRatio(uint256 amountAToken, uint256 amountBToken) internal pure returns (uint256) {
+        require(amountAToken > 0 && amountBToken > 0, "Insufficient token amount to calculate Ratio");
+        return (amountAToken * 1e18) / amountBToken;
+    }
+
+    function _mint(address to, uint256 lpTokens) internal {
+        balances[to] += lpTokens;
+        totalSupply += lpTokens;
+        emit Transfer(address(0), to, lpTokens);
+    }
+
+    function _burn(address from, uint256 lpTokens) private {
+        balances[from] -= lpTokens;
+        totalSupply -= lpTokens;
+        emit Transfer(from, address(0), lpTokens);
+    }
+
+    function _updateReserves(uint112 amountA, uint112 amountB) internal {
+        reserveTokenA += amountA;
+        reserveTokenB += amountB;
+    }
 
     function removeLiquidity() public {}
 
